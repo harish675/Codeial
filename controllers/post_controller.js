a6e0c98bb25df5d6eeb1a49438554a53921473da
Harish Jadhav <harishjadhav675@gmail.com>
2023-06-27 20:19:15 +0530

    cretae other views file

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,10 @@
 
 module.exports.post= function(req,res){
+
+     return res.render('post',{
+
+          title :"post"
+     });
      
-     res.end('<h1>Users Post</h1>');
+     // res.end('<h1>Users Post</h1>');
 } 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,8 @@
 module.exports.profile = function (req,res){
      
-     res.end('<h1>Users Profile</h1>');
+     res.render('profile',{
+            
+           title : 'Profile'
+     });
+    // res.end('<h1>Users Profile</h1>');
 };
