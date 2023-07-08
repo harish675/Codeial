77d4e81548c75bd69bb62247cd77829f675a994d
Harish Jadhav <harishjadhav675@gmail.com>
2023-07-08 23:26:10 +0530

    create sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,64 @@
+const User = require('../models/user');
+//create controller for the profile
 module.exports.profile = function (req,res){
      
-     res.render('profile',{
-            
-           title : 'Profile'
+      res.render('profile',{         
+            title : "Codeial | profile"
+      });
+ }
+
+ //create controller for the sign in page
+module.exports.SignIn = function (req,res){
+     
+      res.render('user_sign_up',{         
+            title : "Codeial | Sign Up"
+      });
+    
+ }
+
+//create controller for the sign up page
+module.exports.SignUp = function (req,res){
+     
+     res.render('user_sign_in',{         
+           title : "Codeial | Sign In"
      });
-    // res.end('<h1>Users Profile</h1>');
+   
 };
+
+
+//get the sign up data
+
+module.exports.create = function(req,res){
+      
+      if(req.body.password != req.boy.confirm_password){
+            return  res.redirect('back');
+      };
+
+      User.findOne({
+            email:req.body.email
+      },function(err,user){
+
+            if(err){
+                  console.log("error in finding user in signing up");
+                  return;
+            }
+            if(!user){
+                  User.create(req.body,function(err,user){
+                        if(err){
+                              console.log("error in creating user");
+                        }
+                        return res.redirect('/users/sign_in');
+                  })
+            }else{
+
+                  return res.redirect('back');
+            }
+      });
+      
+
+}
+
+//sign in and create session for the user
+module.exports.createSession = function(req,res){
+       //todo later
+}
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
  const express =  require('express');
 
 const userController = require('../controllers/users_controller');
-const postController = require ('../controllers/post_controller');
+
+
 const router = express.Router();
 
  router.get('/profile',userController.profile);
- router.get('/post',postController.post);
+ 
+ router.get('/signin',userController.SignIn);
+ router.get('/signup',userController.SignUp);
+ 
+ router.post('/create',userController.create);
+ 
+
 
  module.exports=router;
