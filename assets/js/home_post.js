5a72a92f63ee2d3e423cb36f06145c052a7ca1a4
Harish Jadhav <harishjadhav675@gmail.com>
2023-07-26 22:44:46 +0530

    cretaed method to create a post in DOM

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -14,7 +14,10 @@
                      data:newPostFrom.serialize(),
                      success:function(data){
                            console.log(data);
-                           
+
+                         let newPost = newPostDom(data.data.post);
+                          
+                         $('#post-display>ul').prepend(newPost);
                        },
                        error:function(error){
                          console.log(error.responseText);
@@ -24,7 +27,42 @@
         
     }
      
-    createPost();      
-}
-
 //method to create a post in DOM
+ 
+ let newPostDom = function(post){
+    
+      return $(`<li id="post-${post._id}">
+                  <div id="post-content">
+                   
+                  <small>
+                    <a class="delete-post-button" href="/post/destroy/${post._id}">Delete</a>
+                  </small>
+                   
+                  <p> ${post.user.name}</p>
+                  <p>${post.content}</p>
+                  <!-- Access user information -->
+                  </div>
+                  <div class="post-commnets">
+                     
+              
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment...">
+                                <input type="hidden" name="post" value="${post._id}">
+                                <input type="submit" value="Add Comment">
+                            </form>
+              
+                      <div class="post-comments-list">
+                          <ul id="post-commnets-${post._id}">
+                            
+                          </ul>
+                      </div>
+                  </div>
+                </li>
+              `)
+ }
+
+
+
+
+     createPost();      
+  }
