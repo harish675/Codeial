c8c4a7315ad66a642c6850c08c002ccdf095861b
Harish Jadhav <harishjadhav675@gmail.com>
2023-07-31 16:32:46 +0530

    set up JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,34 @@
+
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+let opts ={
+
+    jwtFromRequest :ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey : 'codeial',
+}
+
+passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
+    
+    
+     User.findById(jwtPayLoad._id)
+     then((user)=>{
+        if(user){
+            return done(null,user)
+        }
+         
+        else{
+              return done(null,false);
+        }  
+     })
+     .catch((err)=>{
+       console.log("Error in finding User in JWT",);
+       return;
+     });
+       
+       
+}));
+
+module.exports = passport;
--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,44 @@
-module.exports.index = function(req,res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comments');
+
+module.exports.index = async function(req,res){
       
+         let posts =  await Post.find({})
+         .sort('-createdAt')
+         .populate('user') //populate user filed
+         .populate({
+               path:'comments',
+               populate:{
+                     path:'user'
+               }
+         });
      return res.json(200,{
         massage:"List of Posts",
-        posts:[]
+        posts:posts
      })
 }
+
+module.exports.destroy = async function(req,res){
+ 
+   const postId = req.params.id;
+
+  try{
+
+     let postDelete = await Post.findByIdAndRemove(postId);
+  
+      if(postDelete){
+          //if post is found delete all comments associated with comments
+           await Comment.deleteMany({post:postId});
+           
+   
+           return res.json(200,{
+               massage:"Post and associated comments deleted successfully!"
+           })
+      }
+
+  }catch(err){
+      return res.json(500,{
+          massage:"Internal Server Error"
+      });
+  }      
+};
