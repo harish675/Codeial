3de5ed0731a843a6f215096772d74cbe64571a96
Harish Jadhav <harishjadhav675@gmail.com>
2023-08-13 12:53:06 +0530

    create like toggle for Post and Comment

--- controllers/likes_controller.js
+++ controllers/likes_controller.js
@@ -1,69 +1,58 @@
-
 const Like = require('../models/like');
 const Post = require('../models/post');
 const Comment = require('../models/comments');
 
+module.exports.toggleLike = async function (req, res) {
+        try {
+                // likes/toggle/?id=abcdef&type=Post
+                let likeable;
+                let deleted = false;
+                if (req.query.type == 'Post') {
+                    likeable = await Post.findById(req.query.id).populate('likes'); 
+                } else {
+                    likeable = await Comment.findById(req.query.id).populate('likes');
+                }
+                
+                // Find the existing like
+                let existingLike = await Like.findOne({
+                      likeable: req.query.id,
+                      onModel: req.query.type,
+                      user: req.user._id,
+                });
 
-//create action for routes
+                // If a like already exists, remove it. Otherwise, create a new like.
+                if (existingLike) {
+                      // Remove the existing like entry from the likeable's likes array
+                      likeable.likes.pull(existingLike._id);
+                      likeable.save();
+                      // Delete the existing like document using the deleteOne method
+                      await Like.deleteOne({
+                          _id: existingLike._id
+                  });
 
-module.exports.toggleLike = async function(req,ress){
-     
-     try{
-         
-          //likes/toggle/?id=abcdef&type=Post
+                deleted = true;
 
-          let likeable;
-          let deleted;
+                } else {
+                    // Create a new like
+                    let newLike = await Like.create({
+                      user: req.user._id,
+                      likeable: req.query.id,
+                      onModel: req.query.type,
+                  });
 
-          if(req.query.type == 'Post'){
-         
-             likeable = await Post.findById(req.query.id).populate('likes');
-
-            
-          }else{
-              
-             likeable = await Comment.findById(req.query.id).populate('likes');
-             
-          }
-          
-          //check if like already  exits
-
-          let existingLike = await Link.findOne({
-              likeable:req.query.id,
-              onModel:req.query.type,
-              user:req.user._id
-          })
-     
-         //if a like all ready exits the delete it else make a new like
-          if(existingLike){
-             
-            likeable.likes.pull(existingLike._id);
-            likeable.save();
-            existingLike.remove();
-            deleted = true;    
-          }
-          else{
-             //else does not exits like
-              let newLike = await Like.create({
-                 user:req.user._id,
-                 likeable:req.query.id,
-                 onModel:req.query.type,
-              });
-
-              likeable.likes.push(newLike._id);
-              likeable.save();
-          }
-      
-        return res.json(200,{
-             message:"Request Successful",
-             data:{
-                 deleted:deleted
-             }
-        })  
-     }catch(err){   
-         console.log(err);
-         return res.json(500,{
-              message:'Internal Sever Error'
-         });    
-     }
-}
+                likeable.likes.push(newLike._id);
+                likeable.save();
+                }
+                return res.json(200, {
+                      message: 'Request Successful',
+                      data: {
+                        deleted: deleted,
+                      },
+                });
+        } catch (err) {
+                console.log(err);
+                return res.json(500, {
+                  message: 'Internal Server Error',
+                });
+        }
+};
